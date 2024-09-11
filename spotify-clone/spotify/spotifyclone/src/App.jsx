9088575485989af9e6559717e8bc12ba5cda7e4e
vishal <mgvishalctr@gmail.com>
2024-09-11 15:28:49 +0530

    onprogress

--- spotify-clone/spotify/spotifyclone/src/App.jsx
+++ spotify-clone/spotify/spotifyclone/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Sidebar from './components/sidebar'
+import Player from './components/Player'
 
 const App = () => {
   return (
     <div>
+      <div className="h-screen bg-black">
+        <div className='h-[90%]-flex'>
+          <Sidebar/>
+        </div>
+        <Player/>
+
+
+      </div>
       
     </div>
   )
