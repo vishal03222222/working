9cc2783bdbc11c4ba3cde1712d77bb038480fbbc
vishal <mgvishalctr@gmail.com>
2024-09-16 12:29:34 +0530

    do

---
+++ roomsbooking/src/components/AdultsDropdown.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AdultsDropdown = () => {
+  return (
+    <div>
+        
+        AdultsDropdown
+    </div>
+  )
+}
+
+export default AdultsDropdown
---
+++ roomsbooking/src/context/RoomContext.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RoomContext = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RoomContext
