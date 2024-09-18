deb6a8e13df444cddd290c68d66fbdffdb6adb36
vishal <mgvishalctr@gmail.com>
2024-09-18 23:19:54 +0530

    do

--- roomsbooking/roomsbooking/src/components/Header.jsx
+++ roomsbooking/roomsbooking/src/components/Header.jsx
@@ -14,8 +14,9 @@ const Header = () => {
     <header 
     className={`${
    header ? 'bg-white py-6 shadow-lg': 'bg-transparent py-8' }fixed 
-   z-50 w-full transition-all duration-300`}>
-    <div className="container mx-auto">
+   z-50 w-full transition-all duration-500`}>
+    <div className="container mx-auto flex flex-col items-center gap-y-6
+    lg:flex-row lg:justify-between lg:gap-y-0 ">
       <a href='/'>
       {header ? (<img src={LogoDark} />):(<img src={Logowhite} />) }
             </a>
--- roomsbooking/roomsbooking/src/components/HeroSlider.jsx
+++ roomsbooking/roomsbooking/src/components/HeroSlider.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import {Swiper,SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import {EffectFade , Autoplay} from 'swiper';
+import img1 from '../assets/img/heroslider/1.jpg'
+import img2 from '../assets/img/heroslider/2.jpg'
+import img3 from '../assets/img/heroslider/3.jpg'
+ 
+const slides=[
+  {
+    title:'your Luxury Hotel For Vacation',
+    bg:img1,
+    btnText: 'Room & Suites'
+  },
+  {
+    title:'your Luxury Hotel For Vacation',
+    bg:img2,
+    btnText: 'Room & Suites'
+  },
+  {
+    title:'your Luxury Hotel For Vacation',
+    bg:img3,
+    btnText: 'Room & Suites'
+  }
+]
+
 
 const HeroSlider = () => {
   return (
-    <div className='heroslider h-[600px] lg:h-[860px] bg-blue-200'>
-      HeroSlider
-    </div>
+    <Swiper className='heroSlider h-[600px] lg:h-[860px]'>
+      {slides.map((slide,index)=>{
+        return(
+          <SwiperSlide className='h-full bg-pink-400'>
+            <div>
+              <div> Just Enjoy and Relax</div>
+            </div>
+            <div className='absolute top-0 w-full h-full'>
+              <img  className='object-cover h-full w-full'src={slide.bg} alt="" />
+            </div>
+
+          </SwiperSlide>
+        )
+      })}
+    </Swiper>
   )
 }
 
