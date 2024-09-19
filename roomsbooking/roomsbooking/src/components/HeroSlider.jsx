e5f65252b259920afcb8aa3cb589e67217a82bdd
vishal <mgvishalctr@gmail.com>
2024-09-19 19:48:21 +0530

    done

--- roomsbooking/roomsbooking/src/components/HeroSlider.jsx
+++ roomsbooking/roomsbooking/src/components/HeroSlider.jsx
@@ -30,15 +30,16 @@ const HeroSlider = () => {
   return (
     <Swiper className='heroSlider h-[600px] lg:h-[860px]'>
       {slides.map((slide,index)=>{
+        const {title,bg,btnText}=slide
         return(
-          <SwiperSlide className='h-full bg-pink-400'>
+          <SwiperSlide className='h-full bg-pink-400 relative justify-center items-center' key={index}>
             <div>
-              <div> Just Enjoy and Relax</div>
+              <div className='uppercase font-tertiary tracking-[6px] mb-5'> Just Enjoy and Relax</div>
             </div>
             <div className='absolute top-0 w-full h-full'>
               <img  className='object-cover h-full w-full'src={slide.bg} alt="" />
             </div>
-
+            <div className='absolute w-full h-full bg-black/70'></div>
           </SwiperSlide>
         )
       })}
