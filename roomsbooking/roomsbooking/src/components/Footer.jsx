100ebe09bee727974d6bff3f851ed2a966e0df99
vishal <mgvishalctr@gmail.com>
2024-09-19 23:16:58 +0530

    done

--- roomsbooking/roomsbooking/src/components/Footer.jsx
+++ roomsbooking/roomsbooking/src/components/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Logowhite from '../assets/img/logo-white.svg'
+
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+   <footer className='bg-primary py-12' >
+
+     <div className='container mx-auto text-white flex justify-between'>
+      <a href="/">
+      <img src={Logowhite} alt="" />
+      </a>
+      copyright &cpy; 2024. All rights reserved.
     </div>
+   </footer>
   )
 }
 
--- roomsbooking/roomsbooking/src/pages/RoomDetails.jsx
+++ roomsbooking/roomsbooking/src/pages/RoomDetails.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import { useContext } from 'react';
+import useParams from 'react-router-dom'
+import Adultsdropdown from '../components/AdultsDropdown';
+import kidsdropdown from '../components/KidsDropdown';
+import checkin from '../components/CheckIn';
+import checkout from '../components/CheckOut';
 
+import Scrolltotop from '../components/ScrollToTop';
+import { Roomcontext } from '../context/RoomContext';
+import { FaCheck } from 'react-icons/fa';
 const RoomDetails = () => {
+   const {rooms} =useContext(Roomcontext);
+    const {id} = useParams();
+  
+
+    const room =rooms.find((room)=>{
+      return room.id ===Number(id)
+    });
+
+    const {name , description,facilaties,imageLg,price}=room
   return (
-    <div>
-      
-    </div>
+    <section className='bg-pink-200'>
+      {/* banner */}
+      <div className='bg-room bg-cover bg-center h-[560px] relative flex
+      justify-center items-ceneter'>
+        <div className='absolute w-full h-full bg-black/70'></div>
+        {/* title */}
+        <h1 className='text-6xl text-white z-20 font-primary text-center'>
+          {name} Details
+        </h1>
+      </div>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row h-full py2'>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
