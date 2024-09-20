0bbb7534abd92c27db85a5db2083826c7bbd7ddc
vishal <mgvishalctr@gmail.com>
2024-09-20 18:21:08 +0530

    don

--- roomsbooking/roomsbooking/src/pages/RoomDetails.jsx
+++ roomsbooking/roomsbooking/src/pages/RoomDetails.jsx
@@ -10,17 +10,17 @@ import Scrolltotop from '../components/ScrollToTop';
 import { Roomcontext } from '../context/RoomContext';
 import { FaCheck } from 'react-icons/fa';
 const RoomDetails = () => {
-   const {rooms} =useContext(Roomcontext);
-    const {id} = useParams();
-  
+  const { rooms } = useContext(Roomcontext);
+  const { id } = useParams();
 
-    const room =rooms.find((room)=>{
-      return room.id ===Number(id)
-    });
 
-    const {name , description,facilaties,imageLg,price}=room
+  const room = rooms.find((room) => {
+    return room.id === Number(id)
+  });
+
+  const { name, description, facilaties, imageLg, price } = room
   return (
-    <section className='bg-pink-200'>
+    <section >
       {/* banner */}
       <div className='bg-room bg-cover bg-center h-[560px] relative flex
       justify-center items-ceneter'>
@@ -32,6 +32,47 @@ const RoomDetails = () => {
       </div>
       <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row h-full py2'>
+          <div className='w-full -full lg:w-[40%] px-6  '>
+            <h2 className='h2'>{name}</h2>
+            <p className='mb-8'>{description}</p>
+            <img  className='mb-8' src={imageLg} alt="" />
+            <div className='mt-21'>
+            <h3 className='h3 mb-3'Room Facilities></h3>
+          
+           <p  className='mb-12'>
+           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+
+...error.corrupti, ratione nemo dolor, magni blanditiis dicta
+aliquid
+totam libero odit dolorem, natus provident ipsam fugiat
+autem.Eum,
+
+laudantium consectetur
+
+           </p>
+
+           <div className='grid grid-cols-3 gap-6 mb-12'>
+            {facilaties.map((item,index)=>{
+              const {name,icon}=item;
+              return <div className='flex-items-center gap-x-3 flex-1'key={index}>
+                <div className='text-3xl text-accent' >{icon}</div>
+                <div className='text-base' >{name}</div>
+              </div>
+            })}
+           </div>
+           </div>
+           
+          </div>
+
+          
+          <div className='w-full -full lg:w-[40%] bg-blue-300'>
+           <div className='py-'>
+            <div>
+              <h3>Your reservation</h3>
+            </div>
+           </div>
+
+          </div>
 
         </div>
       </div>
