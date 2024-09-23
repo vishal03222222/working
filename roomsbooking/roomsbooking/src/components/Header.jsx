10119d687e90d0a20b825853849b1f1d82f834d2
vishal <mgvishalctr@gmail.com>
2024-09-23 16:41:56 +0530

    vbjkl;'

--- roomsbooking/roomsbooking/src/components/Header.jsx
+++ roomsbooking/roomsbooking/src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
   return (
     <header 
     className={`${
-   header ? 'bg-white py-6 shadow-lg': 'bg-transparent py-8' }fixed 
+   header ? ' relative bg-white py-6 shadow-lg': 'bg-transparent py-8' }fixed 
    z-50 w-full transition-all duration-500`}>
     <div className="container mx-auto flex flex-col items-center gap-y-6
     lg:flex-row lg:justify-between lg:gap-y-0 ">
