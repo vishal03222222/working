bb05b6710d4f7884b0d79d0ad29db3fe7298bd9f
vishal <mgvishalctr@gmail.com>
2024-09-25 23:15:09 +0530

    dfgj

--- ecommerceprj/e-mart/src/singles/Mobilesingles.jsx
+++ ecommerceprj/e-mart/src/singles/Mobilesingles.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import { mobileData } from '../store/Info/data/mobiles'
 import { useParams } from 'react-router-dom'
+import Navbar from '../store/components/Navbar'
 const Mobilesingles = () => {
     const {id}=useParams()
     const product=mobileData.find((item)=>item.id)
   return (
+    <>
+    <Navbar/>
     <div className='ind-page'> 
     <div className="ind-image">
         <img src={product.image} alt="" />
     </div>
     <div className="ind-details">
-    <div className="ind-model"><h3>{product.model}</h3></div>
-    <div className="ind-price"><h3>{product.price}</h3></div>
-    <div className="ind-desc"><p>{product.description}</p></div>
+      <div className="ind-company space"><h2>{product.company}</h2></div>
+    <div className="ind-modelspace "><h3>{product.model}</h3></div>
+    <div className="ind-price space"><h3>{product.price}</h3></div>
+    <div className="ind-desc space"><p>{product.description}</p></div>
       
     </div>
+    <button>Add To Cart</button>
     </div>
+    </>
+    
   )
 }
 
