a1fa7d507ac0f28c9ad6caaa814ec47c3216c1f5
vishal <mgvishalctr@gmail.com>
2024-09-26 12:30:50 +0530

    done

--- ecommerceprj/e-mart/src/store/pages/Mobilepage.jsx
+++ ecommerceprj/e-mart/src/store/pages/Mobilepage.jsx
@@ -1,57 +1,59 @@
 import React, { useState } from 'react'
-import { mobileData } from  '../Info/data/mobiles'
+import { mobileData } from '../Info/data/mobiles'
 import Navbar from '../components/Navbar'
 import { Link } from 'react-router-dom'
 const Mobilepage = () => {
-  const [selectedproduct,setselectedproduct]=useState([]);
-  const companyandler=(mango)=>{
-    if(selectedproduct.includes(mango)){
-      setselectedproduct(selectedproduct.filter(item=>item!==mango))
+  const [selectedproduct, setselectedproduct] = useState([]);
+  const companyandler = (mango) => {
+    if (selectedproduct.includes(mango)) {
+      setselectedproduct(selectedproduct.filter(item => item !== mango))
 
-    }else{
-      setselectedproduct([mango])
+    } else {
+      setselectedproduct([...selectedproduct,mango])
     }
 
   }
+  const filterproduct= selectedproduct.length===0?
+  mobileData :mobileData.filter((orange)=>selectedproduct.includes(orange.company))
   return (
     <>
-    <Navbar/>
-    <div className="pro-selected">
-      {mobileData.map((phone)=>{
-        return(
-          <div>
-            <label htmlFor="">
-              <input type="chekbox" 
-              checked={selectedproduct.includes(phone.company)}
-              onChange={()=>companyandler(phone.company)}/>
-              {phone.company}
-            </label>
-          </div>
-        )
-      })}
-
-    </div>
-    <div className="fullpage">
-    <div className='pagesection'>
-
-{mobileData.map((item)=>{
-    return(
-        <div>
-          <Link to={`/mobie/${item.id}`}>
-          <div className='pageimg'>
-                <img src={item.image} alt="" />
+      <Navbar />
+      <div className="pro-selected">
+        {mobileData.map((phone) => {
+          return (
+            <div className='pro-input'>
+              <label htmlFor="">
+                <input type="chekbox"
+                  checked={selectedproduct.includes(phone.company)}
+                  onChange={() => companyandler(phone.company)} />
+                {phone.company}
+              </label>
             </div>
-          </Link>
-            <div className='promodel'>
-              {item.brand},{item.image}
-                
-            </div>
+          )
+        })}
+
+      </div>
+      <div className="fullpage">
+        <div className='pagesection'>
+
+          {filterproduct.map((item) => {
+            return (
+              <div>
+                <Link to={`/mobie/${item.id}`}>
+                  <div className='pageimg'>
+                    <img src={item.image} alt="" />
+                  </div>
+                </Link>
+                <div className='promodel'>
+                  {item.brand},{item.image}
+
+                </div>
+              </div>
+            )
+          })}
+
         </div>
-    )
-})}
-  
-</div>
-    </div>
+      </div>
     </>
   )
 }
