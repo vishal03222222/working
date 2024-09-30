aa223b273923f51c9704b1a7c745d5adcde95f04
vishal <mgvishalctr@gmail.com>
2024-09-30 23:09:28 +0530

    commit

---
+++ snakegame/snakegame/src/components/Gamepieces.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+const Gamepieces = () => {
+    const canvasref=useRef();
+    const snake_speed=10;
+    const [apple,setapple]=useState({ x:180, y:100});
+    const [snake,setsnake]=useState([
+        { x:100,y:50},
+        {x:95 ,y:50}
+    ]);
+    const [direction,setdirection]=useState(null);
+     useEffect (()=>{
+        const canvas=canvasref.current;
+        const ctx=canvas.getContext("2d")
+        
+        const drawsnake=()=>{
+            snake.forEach(()=>{
+                ctx.beginpath();
+                ctx.rect(snakepark.x,snakepark.y,14,14)
+                ctx.fillstyle="#90EE90";
+                ctx.fill()
+                ctx.closepath()
+            })
+        }
+        const drawapple=()=>{
+            ctx.beginpath();
+            ctx.rect(apple.x,apple.y,14,14)
+            ctx.fillstyle="#FF0000";
+            ctx.fill()
+            ctx.closepath()
+        }
+        const movesnake=()=>{
+            if(direction){
+                setsnake((prevsnake)=>{
+                    const newsnake=[...prevsnake];
+                    const snakehead={x:newsnake[0].x,y:newsnake[0].y}
+
+                })
+
+            }
+        }
+        const interval =setInterval(() => {
+            ctx.clearReact(0,0,canvas.width ,canvas.height);
+            drawsnake();
+            drawapple();
+            
+        },100 );
+        return ()=>{
+            clearInterval(interval)
+        };
+
+     },[snake,direction])
+  return (
+    <div>
+      <canvas className='gamecanvas' ref={canvasref} width={750} height={420}></canvas>
+    </div>
+  )
+}
+
+export default Gamepieces
---
+++ snakegame/snakegame/src/components/Gamestate.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import Gamepieces from './Gamepieces';
+
+const Gamestate = () => {
+    const[score,getscore]=useState(0);
+    const[highscore,sethighscore]=useState(parseInt(localStorage.getitem('highscore'))||0);
+    const[gameover,setgameover]=useState(false);
+    const[collisontype,setcollisiontype]=useState("")
+
+  return (
+    <div>
+        <p>score:{score}</p>
+        <p>High score: {highscore}</p>
+        {
+            gameover &&(
+                <div>
+                    <p>Game over! {collision === "wall" ? "You Hit te wall" : "you ate yourself"}</p>
+                    <p>please press enter to reset the game</p>
+                </div>
+            )
+        }
+      {
+
+        !gameover && (
+            <Gamepieces/>
+        )
+      }
+    </div>
+  )
+}
+
+export default Gamestate
