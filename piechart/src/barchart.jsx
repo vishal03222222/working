e93a5ccbedb553d2a973a9fc64686b26209e3b6d
vishal <mgvishalctr@gmail.com>
2024-11-21 10:11:04 +0530

    barcart

--- piechart/src/barchart.jsx
+++ piechart/src/barchart.jsx
@@ -4,11 +4,38 @@ import { AgCharts } from "ag-charts-react";
 
 const getData = () => {
   return [
-    { asset: "Stocks", amount: 60000 },
-    { asset: "Bonds", amount: 40000 },
-    { asset: "Cash", amount: 7000 },
-    { asset: "Real Estate", amount: 5000 },
-    { asset: "Commodities", amount: 3000 },
+    {
+      quarter: "Q1'18",
+      iphone: 140,
+      mac: 16,
+      ipad: 14,
+      wearables: 12,
+      services: 20,
+    },
+    {
+      quarter: "Q2'18",
+      iphone: 124,
+      mac: 20,
+      ipad: 14,
+      wearables: 12,
+      services: 30,
+    },
+    {
+      quarter: "Q3'18",
+      iphone: 112,
+      mac: 20,
+      ipad: 18,
+      wearables: 14,
+      services: 36,
+    },
+    {
+      quarter: "Q4'18",
+      iphone: 118,
+      mac: 24,
+      ipad: 14,
+      wearables: 14,
+      services: 36,
+    },
   ];
 };
 
@@ -18,27 +45,37 @@ const Bargraph = () => {
     title: {
       text: "Portfolio Composition",
     },
-    axes: [
-      {
-        type: "category",
-        position: "bottom",
-        label: {
-          rotation: 45, // Optional: Rotate labels for better readability
-        },
-      },
-      {
-        type: "number",
-        position: "left",
-      },
-    ],
+    
     series: [
       {
         type: "bar",
-        xKey: "asset",
-        yKey: "amount",
-        label: {
-          formatter: ({ value }) => `$${value}`, // Optional: Format the labels as currency
-        },
+        xKey: "quarter",
+        yKey: "iphone",
+        yName: "iPhone",
+      },
+      {
+        type: "bar",
+        xKey: "quarter",
+        yKey: "mac",
+        yName: "Mac",
+      },
+      {
+        type: "bar",
+        xKey: "quarter",
+        yKey: "ipad",
+        yName: "iPad",
+      },
+      {
+        type: "bar",
+        xKey: "quarter",
+        yKey: "wearables",
+        yName: "Wearables",
+      },
+      {
+        type: "bar",
+        xKey: "quarter",
+        yKey: "services",
+        yName: "Services",
       },
     ],
   });
