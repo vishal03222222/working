03fa63fa9932704914f8309aff92bb0931aaf2bb
vishal <mgvishalctr@gmail.com>
2024-11-30 12:21:57 +0530

    push

---
+++ estate/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./utils/Header";
+import Hero from "./utils/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/><Hero/>
+    </div>
+  )
+    
+}
+
+export default App;
