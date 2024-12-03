0d947521c71501e9be7ef0e7850a93375a7c8475
vishal <mgvishalctr@gmail.com>
2024-12-03 21:34:46 +0530

    pus

--- estate/src/utils/Hero.jsx
+++ estate/src/utils/Hero.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
 import "./Hero.css"
+import {HiLocationMarker} from 'react-iocns/hi';
+import Countup from "react-countup"
 const Hero = () => {
   return (
     <section className='hero-wrapper'>
       <div className='paddings innerWidth flexCenter hero-Container'>
-        <div className='hero-left'>
-          left side
+        <div className='flexColStart hero-left'>
+          <div className='hero-title'>
+            <div className='orange-circle'></div>
+            <h1>Discover <br />Most Suitable <br />Property</h1> </div>
+        </div>
+        <div className='flexColStart hero-des'>
+          <span>Find a variety of peroperties that suit  you very easily</span>
+          <span>Forget all difficulties in findina a residence for you</span>
+          <div className='flexCenter search-bar'>
+            <HiLocationMarker color="var(--blue)" size={25}/>
+            <input type="text" />
+            <button className='button'>Search</button>
+          </div>
+            <div className='flexCenter stats'>
+              <div className='flexColStart stat'>
+                <span>
+                  <Countup start={8800} end={9000} duration={4}/>
+                  <span>+</span>
+                </span>
+                <span>Premium products</span>
+              </div>
+            </div>
+
+            <div className='flexCenter stats'>
+              <div className='flexColStart stat'>
+                <span>
+                  <Countup start={1950} end={2000} duration={4}/>
+                  <span>+</span>
+                </span>
+                <span>Happy Customers </span>
+              </div>
+            </div>
+
+            <div className='flexCenter stats'>
+              <div className='flexColStart stat'>
+                <span>
+                  <Countup  end={28} />
+                  <span>+</span>
+                </span>
+                <span>Award Winning</span>
+              </div>
+            </div>
         </div>
         <div className='flexCenter hero-right'>
           <div className='image-cointainer'>
