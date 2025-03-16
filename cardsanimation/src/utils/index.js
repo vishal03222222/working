ffc5b602b27ea19271d7ba18f52e5a74b14d98c9
vishal <mgvishalctr@gmail.com>
2025-03-16 12:10:35 +0530

    commit

--- cardsanimation/src/utils/index.js
+++ cardsanimation/src/utils/index.js
@@ -1,12 +1,26 @@
+// export const rendergradient = (colorId) => {
+//     let gradient = "";
+//     switch (colorId) {
+//         case 1:
+//             gradient = `${"to right" + "," + "#42275a" + "," + "#734b6d"}`;
+//             return gradient;
+//         case 2:
+//             gradient = `${"to right" + "," + "#45b649" + "," + "#a8e063"}`;
+//         default:
+//             return gradient
+//     }
+// }
 export const rendergradient = (colorId) => {
     let gradient = "";
     switch (colorId) {
         case 1:
-            gradient = `${"to right" + "," + "#42275a" + "," + "#734b6d"}`;
-            return gradient;
+            gradient = "to right, #42275a, #734b6d";
+            break; // Added break statement
         case 2:
-            gradient = `${"to right" + "," + "#45b649" + "," + "#a8e063"}`;
+            gradient = "to right, #45b649, #a8e063";
+            break; // Added break statement
         default:
-            return gradient
+            gradient = ""; // Ensuring default always returns a valid value
     }
-}
+    return gradient;
+};
